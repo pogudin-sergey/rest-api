5cb8feb9aff76574ec8d7633104cd892ddc83d7d
Pogudin Sergey <sergey@pogudin.pro>
2024-04-22 11:42:39 +0500

    Update schema verifier

--- server/src/controllers/auto.controller.ts
+++ server/src/controllers/auto.controller.ts
@@ -1,19 +1,11 @@
 import { Request, Response } from 'express';
-import Ajv from 'ajv';
 import Car, { CarType } from '../models/car';
-import logger from '../utils/logger';
-import schemaCreate from './create.controller.schema.json';
-
-const ajv = new Ajv();
-const validateCreate = ajv.compile(schemaCreate);
+import validator from './validators';
 
 export default class AutoController {
   async create(req: Request, res: Response): Promise<void> {
     try {
-      if (!validateCreate(req.body)) {
-        logger.error(validateCreate.errors);
-        throw new Error('Request body validation failed');
-      }
+      if (!validator.isValid(validator.scheme.create, req, res)) return;
 
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       const car = new Car(req.body as CarType);
---
+++ server/src/controllers/validators/index.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import Ajv, { ValidateFunction } from 'ajv';
+import schemaCreate from './create.controller.schema.json';
+import logger from '../../utils/logger';
+
+const ajv = new Ajv();
+
+type Scheme = {
+  [key: string]: ValidateFunction
+};
+
+const scheme: Scheme = {
+  create: ajv.compile(schemaCreate),
+};
+
+export function isValid(scheme: ValidateFunction, req: Request, res: Response): boolean {
+  if (!scheme(req.body)) {
+    logger.error(scheme.errors);
+    res.status(400).json({
+      errors: scheme.errors,
+    });
+
+    return false;
+  }
+
+  return true;
+}
+
+export default {
+  isValid,
+  scheme,
+};
